4bf11bd3137ab5c6fed6afe1c3bbc6ae7b2d8b88
janhavi <janashinde394@gmail.com>
2022-05-13 20:19:16 +1000

    tests passing with help

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
+array_of_numbers = [1, 2, 3]
+
 def square_array(array)
-  # your code here
+ 
+  new_array = []
+   array.each do |int | 
+  new_array << int**2
+  end
+
+  puts new_array
 end
+
+square_array(array_of_numbers)
